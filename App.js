185b12a355b8bcccc0240bdb559b761bd1cc69a0
dale <stewartdale45@yahoo.com>
2022-08-06 23:05:32 -0400

    Implemented the steps to set up Expo, Initializing the build,settin up and Implementing Redux

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+
+// TODO: Set up redux
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={}>
+      <View style={styles.container}>
+        <Text>Lets build UBER!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
