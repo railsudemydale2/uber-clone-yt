ce47dca2a10705eb4e88a48b23c368166aa7ecda
dale <stewartdale45@yahoo.com>
2022-08-12 18:08:56 -0400

    Implemented Google Autocomplete library,built the map component, Build the RideOptionsCard component, build the NavigateCard component, implemented the directiosAPI,implemented the NavFavorites component, implemented the Keyboard Avoiding View, implemented and built the RideOptionsCard component, implemented the Time Travel Calculation, implemented the Price Calculation, built the Menu Button

--- App.js
+++ App.js
@@ -1,25 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { KeyboardAvoidingView, Platform } from 'react-native';
 import { Provider } from 'react-redux';
-
-// TODO: Set up redux
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
-    <Provider store={}>
-      <View style={styles.container}>
-        <Text>Lets build UBER!</Text>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            style={{ flex: 1 }}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? -64 : 0}
+          >
+            <Stack.Navigator>
+              <Stack.Screen
+                name="HomeScreen"
+                component={HomeScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+              <Stack.Screen
+                name="MapScreen"
+                component={MapScreen}
+                options={{
+                  headerShown: false,
+                }}
+              />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
+        </SafeAreaProvider>
+      </NavigationContainer>
+
+      {/* <View style={styles.container}>
         <StatusBar style="auto" />
-      </View>
+      </View> */}
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
